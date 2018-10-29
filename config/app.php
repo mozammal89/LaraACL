8e8c557173390d4bfaca0d36c239bd80a3985c18
Mozammal Hossain <mozammal.bubt89@gmail.com>
2018-10-30 01:02:36 +0600

    install laravelcollective/html

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Collective\Html\HtmlServiceProvider',
 
     ],
 
@@ -208,7 +209,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'FORM' => 'Collective\Html\FormFacade',
+        'HTML' => 'Collective\Html\HtmlFacade',
     ],
 
 ];
