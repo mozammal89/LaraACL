990eb85003ba4e953c0bd0078ca3eb6f2208dc32
Mozammal Hossain <mozammal.bubt89@gmail.com>
2018-11-02 00:31:04 +0600

    complete

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use DB;
 
 class RoleController extends Controller
 {
@@ -11,9 +15,19 @@ class RoleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    function __construct()
+    {
+         $this->middleware('permission:role-list');
+         $this->middleware('permission:role-create', ['only' => ['create','store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
+
     public function index()
     {
-        //
+        $roles = Role::orderBy('id','DESC')->paginate(5);
+        return view('roles.index',compact('roles'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -23,7 +37,8 @@ class RoleController extends Controller
      */
     public function create()
     {
-        //
+       $permission = Permission::get();
+        return view('roles.create',compact('permission'));
     }
 
     /**
@@ -34,7 +49,18 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+
+
+        $role = Role::create(['name' => $request->input('name')]);
+        $role->syncPermissions($request->input('permission'));
+
+
+        return redirect()->route('roles.index')
+                        ->with('success','Role created successfully');
     }
 
     /**
@@ -45,7 +71,13 @@ class RoleController extends Controller
      */
     public function show($id)
     {
-        //
+       $role = Role::find($id);
+        $rolePermissions = Permission::join("role_has_permissions","role_has_permissions.permission_id","=","permissions.id")
+            ->where("role_has_permissions.role_id",$id)
+            ->get();
+
+
+        return view('roles.show',compact('role','rolePermissions'));
     }
 
     /**
@@ -56,7 +88,14 @@ class RoleController extends Controller
      */
     public function edit($id)
     {
-        //
+        $role = Role::find($id);
+        $permission = Permission::get();
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+            ->all();
+
+
+        return view('roles.edit',compact('role','permission','rolePermissions'));
     }
 
     /**
@@ -68,7 +107,22 @@ class RoleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required',
+            'permission' => 'required',
+        ]);
+
+
+        $role = Role::find($id);
+        $role->name = $request->input('name');
+        $role->save();
+
+
+        $role->syncPermissions($request->input('permission'));
+
+
+        return redirect()->route('roles.index')
+                        ->with('success','Role updated successfully');
     }
 
     /**
@@ -79,6 +133,8 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
-        //
+         DB::table("roles")->where('id',$id)->delete();
+        return redirect()->route('roles.index')
+                        ->with('success','Role deleted successfully');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,8 +19,7 @@ class UserController extends Controller
     public function index()
     {
         $data = User::orderBy('id','DESC')->paginate(5);
-        return view('users.index',compact('data'))
-            ->with('i', ($request->input('page', 1) - 1) * 5);
+        return view('users.index',compact('data'));
     }
 
     /**
