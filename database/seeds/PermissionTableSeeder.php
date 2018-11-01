43de042a9db81faffb45bd31f1d09ff76257bc1e
Mozammal Hossain <mozammal.bubt89@gmail.com>
2018-11-01 06:35:26 +0600

    auth

---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+        	'role-list',
+        	'role-create',
+        	'role-edit',
+        	'role-delete',
+        	'prodect-list',
+        	'prodect-create',
+        	'product-edit',
+        	'product-delete'
+        ];
+        foreach ($permissions as $permission) {
+        	Permission::create(['name'=>$permission]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
